ba11555fed128b0d42c382af3f8683e1d3fbce4d
Einar Sommerfeldt <einar.sommerfeldt@gmail.com>
2024-08-03 12:34:45 +0200

    testing mysql in Python

---
+++ db_test.py
@@ -0,0 +1,6 @@
+import mysql.connector 
+  
+mysql.connector.connect(host='localhost', 
+                        database='database', 
+                        user='root', 
+                        password='your password') 
